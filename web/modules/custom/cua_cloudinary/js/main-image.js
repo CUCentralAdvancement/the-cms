5ecffd728ad07c056a64c7de174e194ea259bb51
Alex Finnarn <alex.finnarn@gmail.com>
2021-09-13 19:51:17 -0500

    lots of stuff and puff, huff huff

---
+++ web/modules/custom/cua_cloudinary/js/main-image.js
@@ -0,0 +1,16 @@
+(function ($) {
+  Drupal.behaviors.cua_cloudinary_main_image = {
+    attach: function (context, settings) {
+      var imageLinkNode = document.querySelector('.form-item-field-image-main-0' +
+        ' .image-widget-data .file--image a');
+
+      if (typeof imageLinkNode !== 'undefined') {
+        console.log('Main images');
+        var url = imageLinkNode.getAttribute('href');
+        document.querySelector('.form-item-field-image-main-0 .image-style-thumbnail').setAttribute('src', url);
+        document.querySelector('.form-item-field-image-main-0' +
+          ' .image-style-thumbnail').setAttribute('width', '400');
+      }
+    }
+  };
+}(jQuery));
--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -785,23 +785,6 @@ $settings['entity_update_backup'] = TRUE;
  */
 $settings['migrate_node_migrate_type_classic'] = FALSE;
 
-/**
- * Load local development override configuration, if available.
- *
- * Create a settings.local.php file to override variables on secondary (staging,
- * development, etc.) installations of this site.
- *
- * Typical uses of settings.local.php include:
- * - Disabling caching.
- * - Disabling JavaScript/CSS compression.
- * - Rerouting outgoing emails.
- *
- * Keep this code block at the end of this file to take full effect.
- */
-#
-# if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
-#   include $app_root . '/' . $site_path . '/settings.local.php';
-# }
 //$databases['default']['default'] = array (
 //  'database' => 'drupal9',
 //  'username' => 'postgres',
@@ -842,3 +825,20 @@ $settings['config_exclude_modules'] = [
   'webprofiler',
   'kint',
 ];
+
+/**
+ * Load local development override configuration, if available.
+ *
+ * Create a settings.local.php file to override variables on secondary (staging,
+ * development, etc.) installations of this site.
+ *
+ * Typical uses of settings.local.php include:
+ * - Disabling caching.
+ * - Disabling JavaScript/CSS compression.
+ * - Rerouting outgoing emails.
+ *
+ * Keep this code block at the end of this file to take full effect.
+ */
+if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
+  include $app_root . '/' . $site_path . '/settings.local.php';
+}
